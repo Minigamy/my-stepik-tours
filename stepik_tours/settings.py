095dd5e971fa461474df01492f1d7070895d7d2c
Minigamy <ferdinand-f@mail.ru>
2021-05-15 13:26:06 +0300

    Week1

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%h!e5oz=utvt(e%yg=u_^w0-t6fe@1m02clce3q-^bclmore%-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from tours import views
+from tours.views import custom_handler404, custom_handler500
+
 
 
 urlpatterns = [
@@ -23,4 +25,8 @@ urlpatterns = [
     path('', views.main_view, name='main'),
     path('departure/<str:departure>/', views.departure_view, name='depart'),
     path('tour/<int:id>/', views.tour_view, name='tour'),
+
 ]
+
+handler404 = custom_handler404
+handler500 = custom_handler500
